4fc631345f7a5cedd76437775d0a4cdea5b89e4b
Eli <elikampel@yahoo.com>
2021-11-27 22:53:32 +0200

    moo53

---
+++ assets/scripts/addtocart.js
@@ -0,0 +1,13 @@
+/*let count = document.querySelector('.count');const inc = document.querySelector('.inc');
+const increment = () => {
+  count.textContent++;
+  document.body.appendChild(randomElement)
+};
+inc.addEventListener('click', increment);document.querySelector('.dec').addEventListener('click', () => {
+  +count.textContent === 0 ?
+  (count.textContent = 0) :
+    count.textContent--;    randomElement.innerHTML = `<h1>Bye</h1>`
+});
+const randomElement = document.createElement('p');
+randomElement.innerHTML = `<h1>hi again</h1>`
+console.log(randomElement)*/
